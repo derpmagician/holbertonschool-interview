81185880c71b32cb6af62293c0bb6fddafbb4eab
derpmagician <renzin@gmail.com>
2021-12-02 18:24:28 -0500

    fixing formula

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -28,6 +28,6 @@ def pascal_triangle(n):
             if (j == 0 or j == i):
                 temp_list.append(1)
             else:
-                temp_list.append(pascal_list[i-1][j] + pascal_list[i-1][j])
+                temp_list.append(pascal_list[i-1][j-1] + pascal_list[i-1][j])
         pascal_list.append(temp_list)
     return pascal_list
