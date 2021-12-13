be55345bbe760e7ab4c82b4c7a76ba3a5afe2885
Renzo Perez <renzin@gmail.com>
2021-12-13 15:08:18 -0500

    first commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Method to find number of minimum operations to complete a task.
+"""
+
+
+def minOperations(n):
+    """
+    Only operations are copy and paste.
+    Find the smallest number of operations needed to complete the task.
+    n = number of times to print the letter H.
+    if n is not possible, return 0
+    """
+    minOps = []
