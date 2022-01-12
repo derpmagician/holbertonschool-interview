845c07160cb40387be7a8797c67cdde24a708e67
Renzo Perez <renzin@gmail.com>
2022-01-12 18:36:28 -0500

    adding logic

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,63 +1,76 @@
 #!/usr/bin/python3
 """
-Solves the N queens problem.
+Module for 0x0C. N Queens.
 """
-import sys
+from sys import argv, exit
 
 
-def n_q(t_arr, arr, col, i, n):
+def solveNQueens(n):
     """
-       n_q - Find all posibles solution for N-queen problem and return it
-             in a list
-       @t_arr: temporaly list to store the all points of a posible solution
-       @arr: store all the solution
-       @col: save a colum use for a queen
-       @i: the row of the chess table
-       @n: Number of queens
+    Program that solves the N queens problem
     """
-    if (i > n):
-        arr.append(t_arr[:])
-        return arr
+    res = []
+    queens = [-1] * n
+    # queens is a one-dimension array, like [1, 3, 0, 2] means
+    # index represents row no and value represents col no
 
-    for j in range(n + 1):
-        if i == 0 or ([i - 1, j - 1] not in t_arr and
-                      [i - 1, j + 1] not in t_arr and
-                      j not in col):
-            if i > 1:
-                dia = 0
-                for k in range(2, i + 1):
-                    if ([i - k, j - k] in t_arr) or ([i - k, j + k] in t_arr):
-                        dia = 1
-                        break
-                if dia:
-                    continue
-            t_arr.append([i, j])
-            col.append(j)
-            n_q(t_arr, arr, col, i + 1, n)
-            col.pop()
-            t_arr.pop()
+    def dfs(index):
+        """
+        Recursively resolves the N queens problem
+        """
+        if index == len(queens):  # n queens have been placed correctly
+            res.append(queens[:])
+            return  # backtracking
+        for i in range(len(queens)):
+            queens[index] = i
+            if valid(index):  # pruning
+                dfs(index + 1)
 
-    return arr
+    # check whether nth queens can be placed
+    def valid(n):
+        """
+        Method that checks if a position in the board is valid
+        """
+        for i in range(n):
+            if abs(queens[i] - queens[n]) == n - i:  # same diagonal
+                return False
+            if queens[i] == queens[n]:  # same column
+                return False
+        return True
+
+    # given queens = [1,3,0,2] this function returns
+    # [[0, 1], [1, 3], [2, 0], [3, 2]]
+
+    def make_all_boards(res):
+        """
+        Method that builts the List that be returned
+        """
+        actual_boards = []
+        for queens in res:
+            board = []
+            for row, col in enumerate(queens):
+                board.append([row, col])
+            actual_boards.append(board)
+        return actual_boards
+
+    dfs(0)
+    return make_all_boards(res)
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+    if len(argv) < 2:
+        print('Usage: nqueens N')
         exit(1)
-
     try:
-        n = int(sys.argv[1])
-    except:
-        print("N must be a number")
+        n = int(argv[1])
+    except ValueError:
+        print('N must be a number')
         exit(1)
 
-    if not isinstance(n, int):
-        print("N must be a number")
+    if n < 4:
+        print('N must be at least 4')
         exit(1)
-
-    elif n < 4:
-        print("N must be at least 4")
-        exit(1)
-
-    n_q_arr = n_q([], [], [], 0, n - 1)
-    for i in n_q_arr:
-        print(i)
+    else:
+        result = solveNQueens(n)
+        for row in result:
+            print(row)
