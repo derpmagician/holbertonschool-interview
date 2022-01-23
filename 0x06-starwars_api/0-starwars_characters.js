b94c6b808866c816f81d8fd06b8e6470851372ed
derpmagician <renzin@gmail.com>
2022-01-22 21:35:24 -0500

    fixing code

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,32 +1,18 @@
 #!/usr/bin/node
-
-function order (characters, idx) {
-  if (idx >= characters.length) {
-    return;
-  }
-  request(characters[idx], function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else if (response.statusCode === 200) {
-      const person = JSON.parse(body);
-      console.log(person.name);
-      return order(characters, ++idx);
-    } else {
-      console.log('error ocurred, Status code: ' + response.statusCode);
-    }
-  });
-}
-
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
-const ep = process.argv[2];
-request(url + ep, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const jsobj = JSON.parse(body);
-    order(jsobj.characters, 0);
-  } else {
-    console.log('error ocurred, Status code: ' + response.statusCode);
-  }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/?format=json';
+request.get(url, function (response, body) {
+  const film = (JSON.parse(body.body));
+  // debugging
+  // console.log(film);
+  // console.log(film.characters);
+  character(film.characters);
 });
+const character = (people, i = 0) => {
+  if (i === people.length) return;
+  request.get(people[i], function (response, body) {
+    // console.log(people[i]);
+    console.log(JSON.parse(body.body).name);
+    character(people, i + 1);
+  });
+};
